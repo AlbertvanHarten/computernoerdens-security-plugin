5ccb2af6ed726181a40ea4c635ee1d1982585692
Albert van Harten <albert@computernoerden.dk>
2026-06-30 17:42:01 +0200

    PS C:\websites\computernoerdens-security-plugin> Set up Composer autoloading and project bootstrap Set-Variable : A positional parameter cannot be found that accepts argument 'autoloading'. At line:1 char:1 + Set up Composer autoloading and project bootstrap + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~     + CategoryInfo          : InvalidArgument: (:) [Set-Variable], ParameterBindingException     + FullyQualifiedErrorId : PositionalParameterNotFound,Microsoft.PowerShell.Commands.SetVariableCommand
    PS C:\websites\computernoerdens-security-plugin>

--- computernoerdens-security-plugin.php
+++ computernoerdens-security-plugin.php
@@ -14,3 +14,6 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+require_once __DIR__ . '/vendor/autoload.php';
+
+(new Computernoerden\Security\Core\Application())->boot();
---
+++ src/Core/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Computernoerden\Security\Core;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Main application bootstrap.
+ */
+class Application
+{
+    /**
+     * Boot the plugin.
+     */
+    public function boot()
+    {
+        // Modules will be loaded here.
+    }
+}
